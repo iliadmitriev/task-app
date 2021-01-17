61fe366dcabea76af1482fc9574a86012227abc6
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-01-17 11:30:30 +0300

    added navbar and side menu

---
+++ src/components/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <nav class="blue darken-3">
+      <div class="nav-wrapper">
+        <router-link to="/" class="brand-logo">Tasks</router-link>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+          <i class="material-icons">menu</i>
+        </a>
+        <ul class="right hide-on-med-and-down">
+          <router-link
+              tag="li"
+              to="/"
+              exact
+              active-class="active"
+          >
+            <a href="#">Create</a>
+          </router-link>
+          <router-link
+              tag="li"
+              to="/list"
+              exact
+              active-class="active"
+          >
+            <a href="#">List</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+
+    <ul class="sidenav blue lighten-5" id="mobile-demo">
+      <router-link
+          tag="li"
+          to="/"
+          exact
+          active-class="active"
+      >
+        <a href="#">Create</a>
+      </router-link>
+      <router-link
+          tag="li"
+          to="/list"
+          exact
+          active-class="active"
+      >
+        <a href="#">List</a>
+      </router-link>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  mounted() {
+    document.addEventListener('DOMContentLoaded', ()=> {
+      const elements = document.querySelectorAll('.sidenav')
+      const instances = M.Sidenav.init(elements)
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.brand-logo {
+  padding: 0 2rem;
+}
+</style>
