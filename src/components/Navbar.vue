5641d0b80193486793ec8a5b821222c22d569795
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-07-01 14:00:32 +0300

    refactored router-links style for better vue-router v4 compliance

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,26 +2,37 @@
   <div>
     <nav class="blue darken-3">
       <div class="nav-wrapper">
-        <router-link to="/" class="brand-logo">Tasks</router-link>
+        <router-link
+            :to="{name: 'list'}"
+            exact
+            class="brand-logo"
+        >Tasks</router-link>
+
         <a href="#" data-target="mobile-demo" class="sidenav-trigger">
           <i class="material-icons">menu</i>
         </a>
         <ul class="right hide-on-med-and-down">
           <router-link
-              tag="li"
-              to="/create"
+              v-slot="{ href, isActive, isExactActive, navigate}"
+              custom
+              :to="{name: 'create'}"
               exact
               active-class="active"
           >
-            <a href="#">Create</a>
+            <li :class="[isExactActive && 'active']">
+              <a :href="href" @click="navigate">Create</a>
+            </li>
           </router-link>
           <router-link
-              tag="li"
-              to="/"
+              v-slot="{ href, isActive, isExactActive, navigate}"
+              custom
+              :to="{name: 'list'}"
               exact
               active-class="active"
           >
-            <a href="#">List</a>
+            <li :class="[isExactActive && 'active']">
+              <a :href="href" @click="navigate">List</a>
+            </li>
           </router-link>
         </ul>
       </div>
@@ -29,20 +40,26 @@
 
     <ul ref="sidenav" class="sidenav blue lighten-5" id="mobile-demo">
       <router-link
-          tag="li"
-          to="/create"
+          v-slot="{ href, isActive, isExactActive, navigate}"
+          custom
+          :to="{name: 'create'}"
           exact
           active-class="active"
       >
-        <a href="#">Create</a>
+        <li :class="[isExactActive && 'active']">
+          <a :href="href" @click="navigate">Create</a>
+        </li>
       </router-link>
       <router-link
-          tag="li"
-          to="/"
+          v-slot="{ href, isActive, isExactActive, navigate}"
+          custom
+          :to="{name: 'list'}"
           exact
           active-class="active"
       >
-        <a href="#">List</a>
+        <li :class="[isExactActive && 'active']">
+          <a :href="href" @click="navigate">List</a>
+        </li>
       </router-link>
 
     </ul>
